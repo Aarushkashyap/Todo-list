f0d9e375f6210113da637726193198db3ca27d38
Aarush Kashyap <aarush.kashyap21@gmail.com>
2024-05-11 17:19:39 +0530

    Setting up basic project structure

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="container">
+        <div className="bg-red-600">hey i am red</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
